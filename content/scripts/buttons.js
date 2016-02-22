38cb60a86eb6fb7152ba895722e8324db92229fe
Jake Trower <jakeonaut@gmail.com>
2016-02-21 18:48:16 -0600

    Update buttons.js

--- content/scripts/buttons.js
+++ content/scripts/buttons.js
@@ -106,7 +106,7 @@ function PrintRecipe(){
 		pri.document.write('</head><body>');
 		pri.document.write($("#creatable").html());
 		pri.document.write("</body></html>");
-		pri.print();
+		setTimeout(function(){pri.print();}, 3000);
 		//pri.close();
 	}catch(e){
 		alert(e);
